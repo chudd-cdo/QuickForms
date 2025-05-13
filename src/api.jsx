9ec42cd7452bc132452157bea43a01f5548d942c
ohayoujustin <stephenjustindato@gmail.com>
2025-05-13 15:13:51 +0800

    Update api.jsx

--- src/api.jsx
+++ src/api.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import LocalStorage from "./components/LocalStorage";
 
 const api = axios.create({
-  baseURL: "https://192.168.2.2:5173/api", // ✅ Update with your API URL
+  baseURL: "https://192.168.2.2:82/api", // ✅ Update with your API URL
   withCredentials: true, // ✅ Required for Sanctum
 }); 
 
